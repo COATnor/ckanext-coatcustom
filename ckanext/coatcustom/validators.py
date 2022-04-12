1a79b05772427bb7a6d699d09757f5a658a86522
Francesco Frassinelli <francesco.frassinelli@nina.no>
2022-04-12 14:32:59 +0200

    Serialize fields with JSON

--- ckanext/coatcustom/validators.py
+++ ckanext/coatcustom/validators.py
@@ -3,6 +3,8 @@ import ckan.plugins.toolkit as toolkit
 from ckan.common import config
 from ckan.lib.navl import validators as ckan_validators
 
+import json
+
 def required_custom(key, data, errors, context):
     extras = data.get(('__extras',), {})
     if extras.get('__parent'):
@@ -16,12 +18,14 @@ def str_to_bool(value):
 def bool_to_str(value):
     return str(value)
 
-def multiple_to_string(iterable):
-    if not iterable:  # Missing object
-        iterable = ""
-    if type(iterable) != list:
-        return iterable
-    return ",".join(iterable)
+def commalist_to_json(value):
+    try:
+        return json.loads(value)
+    except ValueError:
+        if type(value) == str:
+            return value.split(',')
+        else:
+            return value
 
 def select_parent_locations(selected_values):
     if not selected_values:
@@ -33,12 +37,10 @@ def select_parent_locations(selected_values):
     sep = " - "
     for selected_value in selected_values:
         selected_label = value_to_label[selected_value]
-        for parts in range(selected_label.count(sep)):
-            section = selected_label.rsplit(sep, parts+1)[0]
-            if section in label_to_value:
-                generated.add(label_to_value[section])
-            else:
-                pass  # this should not happen
+        parts = selected_label.split(sep)
+        for index in range(len(parts)):
+            section = sep.join(parts[:index+1])
+            generated.add(label_to_value[section])
     return list(generated)
 
 def citation_autocomplete(key, data, errors, context):
